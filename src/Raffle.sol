21843e9812f98635f79f5d88c75b0790194a2c15
peterberekvolgyi <berekvolgyipeter@gmail.com>
2024-09-14 20:35:17 +0200

    check block timestamp

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -19,17 +19,21 @@ contract Raffle {
 
     // Lottery Variables
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
     address payable[] private s_players;
 
     /* Events */
     event EnteredRaffle(address indexed player);
 
     /* Functions */
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         if (msg.value <= i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
@@ -37,5 +41,14 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert(); // TODO: implement custom error
+        }
+    }
+
+    /* Getter Functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
 }
